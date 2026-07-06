c5e5b1298f5236dd9cf24d486d453febe57951c3
wesantics <wes.anderson320@gmail.com>
2026-07-06 09:50:40 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import DrawOdds from "./DrawOdds.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <DrawOdds />
+  </React.StrictMode>
+);
